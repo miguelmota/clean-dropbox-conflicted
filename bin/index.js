d673d8765c4556bd7655a68d1744924e51c820cc
Miguel Mota <miguelmota2@gmail.com>
2014-06-20 21:26:48 -0700

    update bin

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,4 @@
 #!/usr/bin/env node
 
 var path  = require('path');
-var clean = require(path.join('../lib/clean'));
-
-var argv = process.argv.slice(2);
-
-if (!argv[0]) {
-    throw new Error('Need directory argument');
-}
-
-var directory = path.resolve(argv[0]);
-
-clean(directory);
+require(path.join('../lib/index'));
